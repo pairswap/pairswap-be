08789fcd21ddcd6ebe855d96bdf647cd5237d5d5
Phat Vo <vphat8415@gmail.com>
2022-03-23 00:16:11 +0700

    Change title

--- src/handler/email.go
+++ src/handler/email.go
@@ -9,11 +9,11 @@ import (
 )
 
 func (h *SupportFormRequest) Send() error {
-	from := mail.NewEmail("Sender", os.Getenv("FROM_EMAIL"))
-	subject := "Have a problem with transaction"
-	to := mail.NewEmail("Receiver", os.Getenv("TO_EMAIL"))
-	plainTextContent := "Example"
-	htmlContent := fmt.Sprintf("<div><p>Name: %s<p><br /><p>Email: %s<p><br /><p>Transaction: %s<p><br /><p>Comment: %s<p><br /></div>", h.Name, h.Email, h.TxURL, h.Comment)
+	from := mail.NewEmail("", os.Getenv("FROM_EMAIL"))
+	subject := "User Form Contact"
+	to := mail.NewEmail("", os.Getenv("TO_EMAIL"))
+	plainTextContent := ""
+	htmlContent := fmt.Sprintf("<div><span>Name: %s</span><br /><span>Email: %s</span><br /><span>Transaction: %s</span><br /><span>Comment: %s</span><br /></div>", h.Name, h.Email, h.TxURL, h.Comment)
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	_, err := client.Send(message)
